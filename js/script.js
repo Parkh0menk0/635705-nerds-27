c85c432eb3dda1e0dec4267756ae1eeaa53c7d8e
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-02-20 19:46:52 +0200

    Завершает стилизацию страниц проекта "Нёрдс"

--- js/script.js
+++ js/script.js
@@ -2,11 +2,6 @@
 
 var swiper = new Swiper('.swiper-container', {
   speed: 800,
-  effect: 'fade',
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: true,
-  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
